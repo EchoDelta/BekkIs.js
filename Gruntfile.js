8f4d010b6ae3c657354ad1b7aa7f3e3313a9b62c
Eirik <eirik@daling.net>
2014-07-19 00:29:24 +0200

    Added browserify and uglify grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,43 @@
+var lessFiles = ['./styles/*.less'],
+    cssDestination = './public/styles.css',
+    browserifyFiles = ['./js/app.js'],
+    jsScriptDestination = './public/script.js';
+
 module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         less: {
-        	files: {
-	        	src: './styles/*.less',
-	        	dest: './public/styles.css'
+            dev: {
+                src: lessFiles,
+                dest: cssDestination
+            },
+        	prod: {
+	        	src: lessFiles,
+	        	dest: cssDestination
         	}
+        },
+        browserify: {
+            dev: {
+                src: browserifyFiles,
+                dest: jsScriptDestination
+            },
+            prod: {
+                src: browserifyFiles,
+                dest: jsScriptDestination
+            }
+        },
+        uglify: {
+            prod: {
+                src: jsScriptDestination,
+                dest: jsScriptDestination
+            }
         }
     });
 
-    grunt.registerTask('default', ['less']);
+    grunt.registerTask('dev', ['less:dev', 'browserify:dev']);
+    grunt.registerTask('prod', ['less:prod', 'browserify:prod', 'uglify:prod']);
 };
